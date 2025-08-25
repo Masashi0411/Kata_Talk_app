b43d9ec3b60e8b89ae1bbda59f9b0b200386355d
Masashi0411 <takeda@LAPTOP-0NBCF516.localdomain>
2025-08-25 23:41:48 +0900

    feat:db:seed←一時的に追加

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,7 +4,7 @@ bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
-# bundle exec rails db:seed   # ← 一時的に追加
+bundle exec rails db:seed   # ← 一時的に追加
 # Render のケースでどちらを使う？
 # プロジェクトには bin/rails が存在しているので、
 # bin/rails db:seed の方が Rails Way で推奨。
