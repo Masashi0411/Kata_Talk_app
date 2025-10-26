921e8eb84361ba070030a900deb3b4b216497dd8
Masashi0411 <takeda@LAPTOP-0NBCF516.localdomain>
2025-10-27 05:48:42 +0900

    feat: devise gemを導入し依存を解決 #54

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_08_24_094121) do
+ActiveRecord::Schema[7.2].define(version: 2025_10_21_205903) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,6 +34,18 @@ ActiveRecord::Schema[7.2].define(version: 2025_08_24_094121) do
     t.text "practice_description", null: false, comment: "TIPの練習用シナリオ/説明文"
     t.index ["scheduled_date"], name: "index_tips_on_scheduled_date", unique: true
     t.check_constraint "char_length(title::text) <= 120", name: "tips_title_length_check"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "posts", "tips"
